43e9fc713354e9ad777306c3815a010b58b921ac
Adam Carpenter <adamcarpenter86@gmail.com>
2017-08-06 18:20:56 -0600

    removed extra character causing problems

--- public/app.js
+++ public/app.js
@@ -175,7 +175,7 @@ $('.close-btn').on('click', function() {
   $('.door').removeClass('open');
 });
 
-$('.sort-btn').on('click', a() {
+$('.sort-btn').on('click', function() {
   if ($(this).text() === 'Sort A-Z') {
     $(this).text('Sort Z-A')
   } else {

524b5cfcf7eae39a0179a130ac7d5146e6f3f1e9
Adam Carpenter <adamcarpenter86@gmail.com>
2017-08-06 19:32:12 -0600

    finished up testing, with everything passing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Garage-Bin
+[Live Site](https://garage--bin.herokuapp.com/)
+
+[Original Spec](https://gist.github.com/robbiejaeger/db7747acf867b1ab180372602146d353)
+
+Completed in 9 hours.
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -87,5 +87,68 @@ describe('Garage Bin Tests', () => {
     });
   });
 
+  describe('PATCH route', () => {
 
+    it('should update a current items cleanliness', (done) => {
+      chai.request(server)
+      .patch('/api/v1/items/2')
+      .send({ cleanliness: 'rancid' })
+      .end((request, response) => {
+        response.should.have.status(201);
+        chai.request(server)
+        .get('/api/v1/items')
+        .end((request, response) => {
+          response.body[1].cleanliness.should.equal('SPARKLING');
+          done();
+        });
+      });
+    });
+
+    it('should deny patch if bad data is sent', (done) => {
+      chai.request(server)
+      .patch('/api/v1/items/2')
+      .send({ reason: 'because' })
+      .end((request, response) => {
+        response.should.have.status(422);
+        chai.request(server)
+        .get('/api/v1/items')
+        .end((request, response) => {
+          response.body[1].reason.should.equal('I am too old now');
+          done();
+        });
+      });
+    });
+  });
+
+  describe('DELETE route', () => {
+
+    it('should delete an item', (done) => {
+      chai.request(server)
+      .get('/api/v1/items')
+      .end((error, response) => {
+        response.body.length.should.equal(4);
+        chai.request(server)
+        .delete('/api/v1/items/258')
+        .end((error, response) => {
+          response.status.should.equal(204);
+          chai.request(server)
+          .get('/api/v1/items')
+          .end((error, response) => {
+            response.body.length.should.equal(3);
+            done();
+          });
+        });
+      });
+    });
+
+    it('should throw an error if ID does not exist', (done) => {
+      chai.request(server)
+      .delete('/api/v1/items/45')
+      .end((error, response) => {
+        response.should.have.status(404);
+        response.error.text.should.equal('item not found.');
+        done();
+      });
+    });
+  });
 });
